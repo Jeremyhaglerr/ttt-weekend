71d3b334afa8f63ba5a90a4090de202e6a9a5961
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-02-03 15:53:17 -0600

    initialize, cache DOM, scaffold init and render

--- js/app.js
+++ js/app.js
@@ -3,16 +3,29 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-
-
-
+squares = []//1.1
+turn = null//1.2
+winner = null//1.3
 /*------------------------ Cached Element References ------------------------*/
 
-
+squareOptions = document.querySelectorAll(".squares")//2.1
+stateMessage = document.querySelector("#message")//2.2 
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 
 
 /*-------------------------------- Functions --------------------------------*/
+init()//3.1
+function init() {
+  //3.2.1
+  let boardArray = Array.from(squareOptions)
 
+
+render()
+}
+
+
+function render() {
+
+}
