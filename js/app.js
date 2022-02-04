3477335d4405a96ef141a6944deb2d95b6746d1e
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-02-04 09:55:21 -0600

    refactor init, add eventListeners, add handleClick

--- js/app.js
+++ js/app.js
@@ -3,29 +3,34 @@
 
 
 /*---------------------------- Variables (state) ----------------------------*/
-squares = []//1.1
-turn = null//1.2
-winner = null//1.3
+let boardArray = []//1.1
+let playerTurn = null//1.2
+let winner = null//1.3
 /*------------------------ Cached Element References ------------------------*/
 
-squareOptions = document.querySelectorAll(".squares")//2.1
+squareOptions = document.querySelectorAll(".square")//2.1
 stateMessage = document.querySelector("#message")//2.2 
-
+resetBtn = document.getElementById("replay")
 /*----------------------------- Event Listeners -----------------------------*/
-
-
-
+squareOptions.forEach(function (idx) {
+  idx.addEventListener('click', handleClick)
+})
+resetBtn.addEventListener('click', init)
 /*-------------------------------- Functions --------------------------------*/
-init()//3.1
-function init() {
-  //3.2.1
-  let boardArray = Array.from(squareOptions)
 
-
-render()
+function init() {//3.1
+  boardArray = []
+  for (let i = 0; i < 9; i++) { boardArray[i] = null; } //3.2.1
+  playerTurn = 1//3.2.2
+  playerTurn = 1//3.2.2
+  render()
 }
-
+init()
 
 function render() {
+
+}
+
+function handleClick() {
 
 }
